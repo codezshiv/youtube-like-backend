a9142a10b510de6662c10f0341bfd1ba8ed3d324
codezshiv <codezeroshiv@gmail.com>
2025-02-08 23:47:18 +0530

    util code done

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// this is promise based method 
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).reject((err) => next(err))
+    }
+}
+
+
+
+
+
+export { asyncHandler }
+
+
+//below is the function or we can say that it is wrapper that we can use so many times in future to handle the async errors in our code. this is method with try and catch and async await
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
