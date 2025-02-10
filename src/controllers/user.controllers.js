7506de8786ccbd2374a135831270fdda722b209b
codezshiv <codezeroshiv@gmail.com>
2025-02-10 17:55:37 +0530

    fix all register controller bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -18,7 +18,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
     
     const {username, email, fullname, password} = req.body
     console.log("email: ", email);
-
+    // console.log(req.body)
     // if(fullname === "")
     // {
     //     throw new ApiError(400, "Fullname cannot be empty")
@@ -28,9 +28,9 @@ import { ApiResponse } from "../utils/ApiResponse.js"
         throw new ApiError(400, "All Field are required!")
     }
 
-    } )
+   
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -38,8 +38,14 @@ import { ApiResponse } from "../utils/ApiResponse.js"
         throw new ApiError(409, "User With email or username already exists")
     }
 
+    // console.log(req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar File is required!")
@@ -73,7 +79,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User Registered Successfully!")
     )
-
+} )
  export {
     registerUser,
 }
