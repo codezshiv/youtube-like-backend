98b3da7daa3451b933348133e818dc4b6d20eaed
codezshiv <codezeroshiv@gmail.com>
2025-02-11 23:21:40 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -44,6 +44,6 @@ const videoSchema = new Schema(
 
 
 
-videoSchema.plugin(mongooseAggregatePaginatef)
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
